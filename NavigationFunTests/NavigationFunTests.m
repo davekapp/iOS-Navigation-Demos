c94071a1a1f89bebe42e2e74b9556dceca6d8a90
David Kapp <davidkapp@coshx.com>
2012-02-04 22:25:57 -0700

    Initial Commit

---
+++ NavigationFunTests/NavigationFunTests.m
@@ -0,0 +1,32 @@
+//
+//  NavigationFunTests.m
+//  NavigationFunTests
+//
+//  Created by David Kapp on 2/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "NavigationFunTests.h"
+
+@implementation NavigationFunTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in NavigationFunTests");
+}
+
+@end
